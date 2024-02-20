f980488df4c36c85be5d8bd270f20b68e5db295d
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-20 16:48:25 +0530

    updated

--- shared/Config.lua
+++ shared/Config.lua
@@ -25,5 +25,5 @@ Config.Target = {
 
 Config.Discord = {
    webhookURL =
-   "https://discord.com/api/webhooks/1191336225940975666/1goKGee7YHVKcqdYzFnAG2QdBTnmwVHDdER6XmCCmtzdf8-0Np2cTdHPmtTpbiTQVXfY"
+   ""
 }
